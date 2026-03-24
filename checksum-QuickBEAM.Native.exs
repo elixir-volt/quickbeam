2002f03c9b8ef755b213ef2ed3293f838596e103
Danila Poyarkov <dev@dannote.net>
2026-03-24 23:11:26 +0300

    Update precompiled NIF checksums for v0.8.0

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,6 +1,6 @@
 [
   {"Elixir.QuickBEAM.Native-v0.8.0-x86_64-linux-gnu.so.tar.gz",
-   "sha256:placeholder"},
+   "sha256:9bd47a5472e6e67c6f7f69d2bb64024c93eb0ae9e46747f6b6b49be752cb5e80"},
   {"Elixir.QuickBEAM.Native-v0.8.0-aarch64-macos-none.so.tar.gz",
-   "sha256:placeholder"}
+   "sha256:50263b852c848e970fa2a8b02f009851cd45f9c05dd268d3d92b9d4f8cd7ee1d"}
 ]
