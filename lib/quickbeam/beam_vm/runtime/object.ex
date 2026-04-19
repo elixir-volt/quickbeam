af3397ba41b8f9a295c503e83665c186fe67c1c3
Danila Poyarkov <dev@dannote.net>
2026-04-19 12:31:32 +0300

    Refactor Object: extract array_indices, enumerable_keys, named proto fns
    - Extract array_indices/1 — was duplicated 3× across keys, keys_from_map, get_own_property_names
    - Extract enumerable_keys/1 — values/entries no longer roundtrip through
      keys() heap wrapping just to unwrap again
    - Simplify keys_from_map to delegate to enumerable_keys
    - Move hasOwnProperty and propertyIsEnumerable from inline lambdas to
      named private functions

--- lib/quickbeam/beam_vm/runtime/object.ex
+++ lib/quickbeam/beam_vm/runtime/object.ex
@@ -14,37 +14,28 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
     Heap.put_obj(ref, %{
       "toString" => {:builtin, "toString", fn _, _ -> "[object Object]" end},
       "valueOf" => {:builtin, "valueOf", fn _, this -> this end},
-      "hasOwnProperty" =>
-        {:builtin, "hasOwnProperty",
-         fn [key | _], this ->
-           case this do
-             {:obj, r} ->
-               data = Heap.get_obj(r, %{})
-               is_map(data) and Map.has_key?(data, key)
-
-             _ ->
-               false
-           end
-         end},
+      "hasOwnProperty" => {:builtin, "hasOwnProperty", &has_own_property/2},
       "isPrototypeOf" => {:builtin, "isPrototypeOf", fn _, _ -> false end},
-      "propertyIsEnumerable" =>
-        {:builtin, "propertyIsEnumerable",
-         fn [key | _], this ->
-           case this do
-             {:obj, r} ->
-               desc = Heap.get_prop_desc(r, key)
-               not match?(%{enumerable: false}, desc)
-
-             _ ->
-               false
-           end
-         end}
+      "propertyIsEnumerable" => {:builtin, "propertyIsEnumerable", &property_enumerable?/2}
     })
 
     proto = {:obj, ref}
     Heap.put_object_prototype(proto)
     proto
   end
+
+  defp has_own_property([key | _], {:obj, r}) do
+    data = Heap.get_obj(r, %{})
+    is_map(data) and Map.has_key?(data, key)
+  end
+
+  defp has_own_property(_, _), do: false
+
+  defp property_enumerable?([key | _], {:obj, r}) do
+    not match?(%{enumerable: false}, Heap.get_prop_desc(r, key))
+  end
+
+  defp property_enumerable?(_, _), do: false
 
   static "keys" do
     keys(args)
@@ -183,8 +174,7 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
     data = Heap.get_obj(ref, %{})
 
     if is_list(data) do
-      keys = Enum.with_index(data) |> Enum.map(fn {_, i} -> Integer.to_string(i) end)
-      Heap.wrap(keys)
+      Heap.wrap(array_indices(data))
     else
       keys_from_map(ref, data)
     end
@@ -195,27 +185,11 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
   end
 
   defp keys_from_map(_ref, list) when is_list(list) do
-    keys = Enum.with_index(list) |> Enum.map(fn {_, i} -> Integer.to_string(i) end)
-    Heap.wrap(keys)
+    Heap.wrap(array_indices(list))
   end
 
   defp keys_from_map(ref, map) when is_map(map) do
-    raw_keys =
-      case Map.get(map, key_order()) do
-        order when is_list(order) -> Enum.reverse(order)
-        _ -> Map.keys(map)
-      end
-
-    all = Runtime.sort_numeric_keys(raw_keys)
-
-    filtered =
-      Enum.filter(all, fn k ->
-        not String.starts_with?(k, "__") and
-          Map.has_key?(map, k) and
-          not match?(%{enumerable: false}, Heap.get_prop_desc(ref, k))
-      end)
-
-    Heap.wrap(filtered)
+    Heap.wrap(enumerable_keys(ref))
   end
 
   defp get_own_property_names([{:obj, ref} | _]) do
@@ -224,7 +198,7 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
     names =
       case data do
         list when is_list(list) ->
-          Enum.with_index(list) |> Enum.map(fn {_, i} -> Integer.to_string(i) end)
+          array_indices(list)
 
         map when is_map(map) ->
           Map.keys(map)
@@ -242,32 +216,42 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
     Heap.wrap([])
   end
 
-  defp raw_keys({:obj, ref}) do
-    case Heap.get_obj(ref, []) do
-      list when is_list(list) -> list
-      _ -> []
+  defp enumerable_keys(ref) do
+    data = Heap.get_obj(ref, %{})
+
+    case data do
+      list when is_list(list) ->
+        array_indices(list)
+
+      map when is_map(map) ->
+        raw = case Map.get(map, key_order()) do
+          order when is_list(order) -> Enum.reverse(order)
+          _ -> Map.keys(map)
+        end
+
+        Runtime.sort_numeric_keys(raw)
+        |> Enum.filter(fn k ->
+          not String.starts_with?(k, "__") and
+            Map.has_key?(map, k) and
+            not match?(%{enumerable: false}, Heap.get_prop_desc(ref, k))
+        end)
+
+      _ ->
+        []
     end
   end
 
   defp values([{:obj, ref} | _]) do
-    ks = raw_keys(keys([{:obj, ref}]))
     map = Heap.get_obj(ref, %{})
-    vals = Enum.map(ks, fn k -> Map.get(map, k) end)
-    Heap.wrap(vals)
+    Heap.wrap(Enum.map(enumerable_keys(ref), fn k -> Map.get(map, k) end))
   end
 
   defp values([map | _]) when is_map(map), do: Map.values(map)
   defp values(_), do: []
 
   defp entries([{:obj, ref} | _]) do
-    ks = raw_keys(keys([{:obj, ref}]))
     map = Heap.get_obj(ref, %{})
-
-    pairs =
-      Enum.map(ks, fn k ->
-        Heap.wrap([k, Map.get(map, k)])
-      end)
-
+    pairs = Enum.map(enumerable_keys(ref), fn k -> Heap.wrap([k, Map.get(map, k)]) end)
     Heap.wrap(pairs)
   end
 
@@ -375,4 +359,8 @@ defmodule QuickBEAM.BeamVM.Runtime.Object do
   end
 
   defp get_own_property_descriptor(_), do: :undefined
+
+  defp array_indices(list) do
+    list |> Enum.with_index() |> Enum.map(fn {_, i} -> Integer.to_string(i) end)
+  end
 end
