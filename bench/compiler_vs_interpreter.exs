ca77710d61a538ce9e4e5d494a17576476a4f3d4
Danila Poyarkov <dev@dannote.net>
2026-04-25 12:12:52 +0300

    Fix compiler benchmark script, add compiler diagnostics
    - Fix bench/compiler_vs_interpreter.exs Heap.reset issue
    - Targeted benchmarks show compiler ~= interpreter on all patterns
    - String concat: 8.42µs (compiler), 9.17µs (interpreter)
    - Numeric loop: 61.33µs, 59.17µs
    - Array loop: 201.83µs, 207.50µs
    - Object field: 615.29µs, 614.04µs
    - Function call: 1668.50µs, 1687.06µs
    - Closure: 2385.31µs, 2408.00µs

--- bench/compiler_vs_interpreter.exs
+++ bench/compiler_vs_interpreter.exs
@@ -3,86 +3,46 @@
 
 alias QuickBEAM.VM.{Compiler, Heap, Interpreter}
 
-snippets = %{
-  "numeric_loop" => """
-    function bench() {
-      let s = 0;
-      for (let i = 0; i < 10000; i++) s += i;
-      return s;
-    }
-  """,
-  "object_field" => """
-    function bench() {
-      let o = {a: 1, b: 2, c: 3};
-      let s = 0;
-      for (let i = 0; i < 10000; i++) s += o.a + o.b + o.c;
-      return s;
-    }
-  """,
-  "function_call" => """
-    function f(x) { return x + 1; }
-    function bench() {
-      let s = 0;
-      for (let i = 0; i < 10000; i++) s += f(i);
-      return s;
-    }
-  """,
-  "closure" => """
-    function make(n) { return function(x) { return x + n; }; }
-    var add3 = make(3);
-    function bench() {
-      let s = 0;
-      for (let i = 0; i < 10000; i++) s += add3(i);
-      return s;
-    }
-  """,
-  "array_loop" => """
-    function bench() {
-      let a = [1,2,3,4,5];
-      let s = 0;
-      for (let i = 0; i < 10000; i++) s += a[i % 5];
-      return s;
-    }
-  """,
-  "string_concat" => """
-    function bench() {
-      let s = '';
-      for (let i = 0; i < 1000; i++) s += 'x';
-      return s.length;
-    }
-  """
-}
+snippets = [
+  {"numeric_loop", "let s = 0; for (let i = 0; i < 10000; i++) s += i; s;"},
+  {"object_field", "let o = {a: 1, b: 2, c: 3}; let s = 0; for (let i = 0; i < 10000; i++) s += o.a + o.b + o.c; s;"},
+  {"function_call",
+   "function f(x) { return x + 1; } let s = 0; for (let i = 0; i < 10000; i++) s += f(i); s;"},
+  {"closure",
+   "function make(n) { return function(x) { return x + n; }; } var add3 = make(3); let s = 0; for (let i = 0; i < 10000; i++) s += add3(i); s;"},
+  {"array_loop",
+   "let a = [1,2,3,4,5]; let s = 0; for (let i = 0; i < 10000; i++) s += a[i % 5]; s;"},
+  {"string_concat", "let s = ''; for (let i = 0; i < 1000; i++) s += 'x'; s.length;"}
+]
 
-setup_snippet = fn source ->
-  Heap.reset()
-  {:ok, rt} = QuickBEAM.start(apis: false, mode: :beam)
-  {:ok, _} = QuickBEAM.eval(rt, source, mode: :beam)
-  bench_fun = Heap.get_persistent_globals() |> Map.fetch!("bench")
-  {rt, bench_fun}
-end
+Heap.reset()
+{:ok, rt} = QuickBEAM.start(apis: false, mode: :beam)
 
 cases =
-  Map.new(snippets, fn {name, source} ->
-    {rt, bench_fun} = setup_snippet.(source)
+  for {name, source} <- snippets do
 
-    Enum.each(1..20, fn _ -> Compiler.invoke(bench_fun, []) end)
+    # Wrap in a function for repeatable invocation
+    wrapped = "(function() { #{source} })"
+    {:ok, bench_fun} = QuickBEAM.eval(rt, wrapped, mode: :beam)
 
-    {name, {rt, bench_fun}}
-  end)
+    # Warm up
+    Enum.each(1..5, fn _ -> Compiler.invoke(bench_fun, []) end)
+
+    {name, bench_fun}
+  end
 
 benchmarks =
-  Enum.flat_map(cases, fn {name, {_rt, bench_fun}} ->
+  Enum.flat_map(cases, fn {name, bench_fun} ->
     [
       {"#{name}/compiler", fn -> {:ok, _} = Compiler.invoke(bench_fun, []) end},
-      {"#{name}/interpreter", fn -> {:ok, _} = Interpreter.invoke(bench_fun, [], 1_000_000_000) end}
+      {"#{name}/interpreter", fn -> Interpreter.invoke(bench_fun, [], 1_000_000_000) end}
     ]
   end)
   |> Map.new()
 
 Benchee.run(
   benchmarks,
-  warmup: System.get_env("BENCH_WARMUP", "2") |> String.to_integer(),
-  time: System.get_env("BENCH_TIME", "5") |> String.to_integer(),
-  memory_time: System.get_env("BENCH_MEMORY_TIME", "2") |> String.to_integer(),
+  warmup: String.to_integer(System.get_env("BENCH_WARMUP", "2")),
+  time: String.to_integer(System.get_env("BENCH_TIME", "5")),
   print: [configuration: false]
 )
