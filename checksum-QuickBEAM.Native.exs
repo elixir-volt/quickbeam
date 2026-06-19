733ae47449aefc0ad8c049dcb99e73558aa565c0
Danila Poyarkov <dev@dannote.net>
2026-06-19 18:30:38 +0200

    Update precompiled NIF checksums for v0.10.18

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,8 +1,8 @@
 [
-  {"Elixir.QuickBEAM.Native-v0.10.17-x86_64-linux-gnu.so.tar.gz",
-   "sha256:981d2c110edbc5acc21fa2c4f32080574301f88895ffea3a017aaa09078da87c"},
-  {"Elixir.QuickBEAM.Native-v0.10.17-aarch64-linux-gnu.so.tar.gz",
-   "sha256:c5fedb626c5a48c733c69493c0a09004736ac7559e750e68f22be4ac89cee535"},
-  {"Elixir.QuickBEAM.Native-v0.10.17-aarch64-macos-none.so.tar.gz",
-   "sha256:f044fc1e24960f4aa6239ab9d1ccd6a3201305710105689a2b0260c1cb3693d8"}
+  {"Elixir.QuickBEAM.Native-v0.10.18-x86_64-linux-gnu.so.tar.gz",
+   "sha256:80388bf4e69afc6aa7f3afabbbdc292e6506c4cdd0078c062c79d4f6984e784c"},
+  {"Elixir.QuickBEAM.Native-v0.10.18-aarch64-linux-gnu.so.tar.gz",
+   "sha256:4ff1087089932bd957a9bf547cf768668186095dc30cc5b590058b21c6cab29d"},
+  {"Elixir.QuickBEAM.Native-v0.10.18-aarch64-macos-none.so.tar.gz",
+   "sha256:5e20147c4720bbb0173771429bc489c81669c43464b9e79b06b636f466969cc6"}
 ]
