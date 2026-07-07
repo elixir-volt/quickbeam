85cb844054fc703da35a729e81a653295e5ef057
Danila Poyarkov <dev@dannote.net>
2026-07-07 19:26:04 +0200

    Update precompiled NIF checksums for v0.10.19

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,5 +1,8 @@
 [
-  {"Elixir.QuickBEAM.Native-v0.10.19-x86_64-linux-gnu.so.tar.gz", "sha256:PLACEHOLDER"},
-  {"Elixir.QuickBEAM.Native-v0.10.19-aarch64-linux-gnu.so.tar.gz", "sha256:PLACEHOLDER"},
-  {"Elixir.QuickBEAM.Native-v0.10.19-aarch64-macos-none.so.tar.gz", "sha256:PLACEHOLDER"}
+  {"Elixir.QuickBEAM.Native-v0.10.19-x86_64-linux-gnu.so.tar.gz",
+   "sha256:0335c896c65ec58d2c531998b5641a047357557c987eebab354cb43fd3ae79dd"},
+  {"Elixir.QuickBEAM.Native-v0.10.19-aarch64-linux-gnu.so.tar.gz",
+   "sha256:d7b4ac0894dbc1cca9dd6499e64ffbc298bdf079f2efcb93f351aaa50107775a"},
+  {"Elixir.QuickBEAM.Native-v0.10.19-aarch64-macos-none.so.tar.gz",
+   "sha256:19e1a49f16c0eb6283a83d93d58a4b73f7bb5b89f6f5d5f38c395fca29e1ed46"}
 ]
