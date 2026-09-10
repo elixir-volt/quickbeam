30bec3dfece33200c615f29333f93ca2de66413e
Danila Poyarkov <dev@dannote.net>
2026-09-10 11:53:53 +0200

    Update precompiled NIF checksums for v0.11.1

--- checksum-QuickBEAM.Native.exs
+++ checksum-QuickBEAM.Native.exs
@@ -1,6 +1,6 @@
 [
-  {"Elixir.QuickBEAM.Native-v0.11.1-x86_64-linux-gnu.so.tar.gz", "sha256:PLACEHOLDER"},
-  {"Elixir.QuickBEAM.Native-v0.11.1-aarch64-linux-gnu.so.tar.gz", "sha256:PLACEHOLDER"},
-  {"Elixir.QuickBEAM.Native-v0.11.1-aarch64-macos-none.so.tar.gz", "sha256:PLACEHOLDER"},
-  {"Elixir.QuickBEAM.Native-v0.11.1-x86_64-windows-gnu.dll.tar.gz", "sha256:PLACEHOLDER"}
+  {"Elixir.QuickBEAM.Native-v0.11.1-x86_64-linux-gnu.so.tar.gz", "sha256:275a85357b88d3ee42ffbbee40ea563cdfec6b01b704fde90ff27b5851001adf"},
+  {"Elixir.QuickBEAM.Native-v0.11.1-aarch64-linux-gnu.so.tar.gz", "sha256:2e5d66b1e81b655b680b4d9bd8a483e1295133a0723383431d9ee79fb7d44857"},
+  {"Elixir.QuickBEAM.Native-v0.11.1-aarch64-macos-none.so.tar.gz", "sha256:2cb13cffe8931a69d7d28d0cd88592f00c3a26cfe10984ac56c5364f7dfdc3d3"},
+  {"Elixir.QuickBEAM.Native-v0.11.1-x86_64-windows-gnu.dll.tar.gz", "sha256:735015879a774867cd0b25895b34b221e3c6b51da76eb66baed8194ae4a5bbc8"}
 ]
